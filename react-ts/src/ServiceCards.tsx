4cd02a636d449dabbdbcb7d79c893f7d04add90a
barreledout <rcalisaan7@gmail.com>
2024-11-27 18:46:06 -0800

    Started working on service cards.

--- react-ts/src/ServiceCards.tsx
+++ react-ts/src/ServiceCards.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ServiceCards = () => {
   return (
-    <div className="">
-      <div></div>
-    </div>
+    <section className="flex flex-col mt-10">
+      {/* First Service Card */}
+      <div className="flex gap-4 relative max-h-[130px] mx-1 bg-red-400">
+        <div className="w-[200px]">
+          <img
+            src="/service_svgs/undraw_analysis_dq08.svg"
+            alt="Home estimate image"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="grid gap-2 text-xs ">
+          <div>
+            <h3>View the Price of Your Home</h3>
+            <p>
+              Get an estimate on your home based on comparable properties
+              similar to yours.
+            </p>
+          </div>
+          <Link to="/home_value_estimator">
+            <span>Get Home Estimate</span>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
