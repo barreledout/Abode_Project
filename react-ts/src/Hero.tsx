c466f005ff54d753866aa17763ac911fb3684d16
barreledout <rcalisaan7@gmail.com>
2024-11-27 17:59:33 -0800

    Proper sematics and ARIA practice with the contrast

--- react-ts/src/Hero.tsx
+++ react-ts/src/Hero.tsx
@@ -15,13 +15,14 @@ const HeroImg = () => {
       <img
         src="/heroHome1.jpg"
         alt="Home page daytime home."
-        className="min-w-[200px] w-full max-h-[400px] object-cover brightness-75"
+        className="min-w-[200px] w-full max-h-[400px] object-cover brightness-75 "
       />
-      <div className="max-w-[400px] font-jost font-[700] text-[1.1rem] text-pretty text-sm sm:text-[1.5em] sm:leading-9 md:leading-10 md:text-[2em] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2  text-white">
-        <p className="text-center">
-          Property value insight, <br className="hidden sm:block" /> Investment
+
+      <div className="max-w-[400px] font-jost font-[700] text-[1.1rem] text-pretty text-sm leading-normal mq400w:text-nowrap mq400w:text-[1.2em] mq500w:text-[1.3em] sm:text-[1.5em] sm:leading-9 md:leading-10 md:text-[2em] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2  text-white">
+        <h2 className="text-center contrast-100">
+          Property value insight, <br className=" sm:block"/> Investment
           success.
-        </p>
+        </h2>
       </div>
     </div>
   );
