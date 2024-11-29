7077f7ca66fa947f8e51ee094c3b18e8ff6ddb3b
barreledout <rcalisaan7@gmail.com>
2024-11-29 14:39:20 -0800

    Made a service logo component so I can combine imgs easier.

---
+++ react-ts/src/Service_section/logos.tsx
@@ -0,0 +1,54 @@
+import { cn } from "../lib/utils";
+
+interface LogoProps {
+  className?: string;
+}
+
+export const HomeEstimateLogo = ({ className }: LogoProps) => {
+  return (
+    <div>
+      <img
+        src="../../../service_svgs/HomeEstimate.svg"
+        alt=""
+        className={cn(`relative top-3 w-full h-full z-20`)}
+      />
+      <img
+        src="../../../service_svgs/blob3.svg"
+        alt=""
+        className={cn(`absolute z-10 top-[-3px] right-3`, className)}
+      />
+    </div>
+  );
+};
+export const MarketAnalysisLogo = ({ className }: LogoProps) => {
+  return (
+    <div>
+      <img
+        src="../../../service_svgs/MarketAnalysis.svg"
+        alt=""
+        className={cn(`relative top-3 w-full h-full z-20`)}
+      />
+      <img
+        src="../../../service_svgs/blob2.svg"
+        alt=""
+        className={cn(`absolute z-10 top-[-12px]`, className)}
+      />
+    </div>
+  );
+};
+export const InvestmentLogo = ({ className }: LogoProps) => {
+  return (
+    <div>
+      <img
+        src="../../../service_svgs/InvestmentAnalysis.svg"
+        alt=""
+        className={cn(`relative top-3 w-full h-full z-20`)}
+      />
+      <img
+        src="../../../service_svgs/blob1.svg"
+        alt=""
+        className={cn(`absolute z-10 top-[-12px]`, className)}
+      />
+    </div>
+  );
+};
