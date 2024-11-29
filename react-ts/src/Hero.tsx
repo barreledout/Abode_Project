b720ce199eed834fbdc68455ddbc2712325a2835
barreledout <rcalisaan7@gmail.com>
2024-11-28 23:12:14 -0800

    Changed the md and large query text font size

--- react-ts/src/Hero.tsx
+++ react-ts/src/Hero.tsx
@@ -7,11 +7,7 @@ const HeroImg = () => {
   });
 
   return (
-    <div
-      className="w-full h-full relative shadow-lg"
-      
-      ref={ref}
-    >
+    <div className="w-full h-full relative shadow-lg" ref={ref}>
       <img
         src="/heroHome1.jpg"
         alt="Home page daytime home."
@@ -19,7 +15,7 @@ const HeroImg = () => {
       />
 
       <div className="max-w-[400px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2">
-        <h2 className=" text-white text-center contrast-100 font-jost font-[700] text-[1.1rem] text-pretty text-sm leading-normal mq400w:text-nowrap mq400w:text-[1.2em] mq500w:text-[1.3em] sm:text-[1.5em] sm:leading-9 md:leading-10 md:text-[2em] 2xl:text-5xl">
+        <h2 className=" text-white text-center contrast-100 font-jost font-[700] text-[1.1rem] text-pretty text-sm leading-normal mq400w:text-nowrap mq400w:text-[1.2em] mq500w:text-[1.3em] sm:text-[1.5em] sm:leading-9 md:leading-10 md:text-4xl  2xl:text-5xl">
           Property value insight, <br className=" sm:block" /> Investment
           success.
         </h2>
