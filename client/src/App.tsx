a75927ec725f648c9f78ed4eadebfd7e994b6d05
barreledout <rcalisaan7@gmail.com>
2024-11-30 12:27:43 -0800

    renamed client side folder to "client" from "react-ts"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Faq from "./Faq";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import HeroImg from "./Hero";
-import ServiceCards from "./ServiceCards";
+import ServiceCards from "./Service_section/ServiceCards";
 
 const Home = () => {
   return (
     <section>
       <HeroImg />
-      <div className="bg-gray-300/20">
+      <div className="bg-gray-300/45 ">
         <ServiceCards />
       </div>
     </section>
