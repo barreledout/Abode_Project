a5d39f3a264d2190014c0e5d12e6b83f69b6523b
barreledout <rcalisaan7@gmail.com>
2024-12-08 13:13:13 -0800

    removed google api street view, implementing it was beyond me. Maybe in the future

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
-
-
+import { error } from "console";
 
 const app = express();
 app.use(cors());
@@ -13,14 +12,15 @@ const PORT = 5000;
 app.post("/homeData", async (req: Request, res: Response) => {
   const { Address, PropertyType, Radius, Comparables } = req.body;
 
-  const RENTCAST_TOKEN = (process.env.RENTCAST_TOKEN as string)
-  console.log(RENTCAST_TOKEN);
-  // Checks if the api key is present.
+  const RENTCAST_TOKEN = process.env.RENTCAST_TOKEN as string;
+
+  // Checks if the api key is not undefined.
   if (!RENTCAST_TOKEN) {
     throw new Error("RentCast API key is missing or undefined");
   }
 
-  const url = `https://api.rentcast.io/v1/avm/value?address=${encodeURIComponent(
+  // RentCast API
+  const rentCast_url = `https://api.rentcast.io/v1/avm/value?address=${encodeURIComponent(
     Address
   )}&propertyType=${encodeURIComponent(
     PropertyType
@@ -34,13 +34,18 @@ app.post("/homeData", async (req: Request, res: Response) => {
   };
 
   try {
-    const response = await fetch(url, options);
-    const data = await response.json();
-    res.json(data);
+    const [rentCastResponse] = await Promise.all([
+      fetch(rentCast_url, options),
+    ]);
+
+    const rentCastData = await rentCastResponse.json();
+    res.json(rentCastData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch home data." });
   }
 });
+
+
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
