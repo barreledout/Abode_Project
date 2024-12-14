1067b03c06250510153fd054ad92d4bd02b716d0
barreledout <rcalisaan7@gmail.com>
2024-12-13 22:27:31 -0800

    Update the error message to also have the detailed error.

--- client/src/ErrorScreen.tsx
+++ client/src/ErrorScreen.tsx
@@ -1,29 +1,40 @@
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { IoReload } from "react-icons/io5";
 
 type ErrorProps = {
   statusCode: number;
+  errorMessage: string | null;
 };
 
-const ErrorScreen = (statusCode: ErrorProps) => {
-  return (
-    <div className="flex flex-col justify-center items-center mt-20 relative">
-      <div className={`grid justify-center items-center`}>
-        <h1
-          className={`font-geistSans text-7xl text-slate-700/100`}
-        >{`${statusCode.statusCode}`}</h1>
-        <p className={`font-geistSans text-slate-700/100`}>
-          There was an error!
-        </p>
-        <button
-          type="button"
-          className="font-geistSans bg-blue-300/30 hover:bg-blue-300/30 py-1 my-6 px-4 rounded-md max-w-[170px] mx-auto shadow-md font-[500] text-black lg:min-w-[300px] lg:text-lg"
-        >
-          <Link to="/">Go Back</Link>
-        </button>
-      </div>
-    </div>
-  );
-};
+const ErrorScreen = forwardRef<HTMLElement, ErrorProps>(
+  ({ statusCode, errorMessage }, ref) => {
+    const handleRefresh = () => {
+      window.location.reload();
+    };
+
+    return (
+      <section ref={ref}>
+        <div className="flex flex-col justify-center items-center mt-20 relative">
+          <div className={`grid justify-center items-center text-center mx-2`}>
+            <h1
+              className={`font-geistSans text-7xl text-slate-700/100`}
+            >{`${statusCode}`}</h1>
+            <p className={`font-geistSans text-slate-700/100`}>
+              {`${errorMessage}`}
+            </p>
+            <button
+              type="button"
+              className={`font-geistSans bg-blue-300/30 hover:bg-blue-300/30 py-1 my-6 px-4 rounded-md max-w-[170px] mx-auto shadow-md font-[500] text-black  lg:text-lg`}
+              onClick={handleRefresh}
+            >
+              <IoReload className={``} />
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+);
 
 export default ErrorScreen;
