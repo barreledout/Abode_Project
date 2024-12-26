ab5d82819859b518c329c94cd34c4b46cbcc6217
barreledout <rcalisaan7@gmail.com>
2024-12-25 21:50:44 -0800

    Updated the fetching of rentcast and supabase data.

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const PORT = 5000;
 
-// RENTCAST API & Supabase
+// Endpoint for home value estimate page.
 app.post("/homeData", async (req: Request, res: Response) => {
   const { Address, PropertyType, Radius, Comparables } = req.body;
 
@@ -47,40 +47,50 @@ app.post("/homeData", async (req: Request, res: Response) => {
 
       // Check if reset date has passed to update the request count back to 0.
       const currentDate: Date = new Date();
-      if (new Date(resetDate) < currentDate) {
+      if (new Date(resetDate) <= currentDate) {
         await updateRequestCount(0, rowId);
+      }
+
+      // Check if request_count from DB has yet to reach the limit of 50.
+      if (requestCount && requestCount < 50) {
+        // fetch to RentCast API.
+        const rentCastResponse = await fetch(rentCast_url, options);
+        const rentCastData = await rentCastResponse.json();
+
+        // send data back to users.
+        res.json(rentCastData);
+
+        // update the request_count in database
+        await updateRequestCount(requestCount + 1, rowId);
+
+        // Send error if request_count has reached its limit of 50.
       } else {
-        // Check if request_count from DB has yet to reach the limit of 50.
-        if (requestCount && requestCount < 50) {
-          // fetch to RentCast API.
-          const rentCastResponse = await fetch(rentCast_url, options);
-          const rentCastData = await rentCastResponse.json();
-
-          // send data back to users.
-          res.json(rentCastData);
-
-          // update the request_count in database
-          await updateRequestCount(requestCount + 1, rowId);
-
-          // Send error if request_count has reached its limit of 50.
-        } else {
-          console.log("Successfully prevented fetching data.");
-          console.log("request count:", requestCount);
-          res
-            .status(500)
-            .json({
-              message: `API usage amount has reached the monthly limit. Try again after ${new Date(
-                resetDate
-              ).toLocaleDateString("en-US")}.`,
-              requestAmount: (requestCount / 50) * 100
-            });
-        }
+        console.log("Successfully prevented fetching data.");
+        console.log("request count:", requestCount);
+        res.status(500).json({
+          message: `API usage amount has reached the monthly limit. Try again after ${new Date(
+            resetDate
+          ).toLocaleDateString("en-US")}.`,
+          requestAmount: (requestCount / 50) * 100,
+        });
       }
     }
   } catch (error) {
     res.status(500).json("Failed to fetch home data.");
   }
 });
+
+// Endpoint for market data page.
+app.post("/marketData"),
+  async (req: Request, res: Response) => {
+    const { zipCode, dataType, historyRange } = req.body;
+    const RENTCAST_TOKEN = process.env.RENTCAST_TOKEN as string;
+
+    // Checks if the api key is not undefined.
+    if (!RENTCAST_TOKEN) {
+      throw new Error("RentCast API key is missing or undefined");
+    }
+  };
 
 interface supabaseDataRow {
   request_count: number;
@@ -119,6 +129,30 @@ const updateRequestCount = async (newRequestCount: number, id: string) => {
     console.error("Failed to update the request count", error);
     return `Failed to update the request count. Error: ${error}`;
   }
+};
+
+// Updating the reset date in database
+const updateResetDate = async (newResetDate: Date) => {
+  const { error } = (await supabase
+    .from("api_limit")
+    .update({ reset_date: newResetDate })
+    .select()) as {
+    error: unknown;
+  };
+
+  if (error) {
+    return `Failed to update the reset date. Error: ${error}`;
+  }
+};
+
+// Checking if we need to update to next reset date if current date <= reset date.
+const checkResetDate = (date: Date) => {
+  // check if the month has 28 (29), 30, or 31 days.
+  checkAmountOfDays(date);
+};
+
+const checkAmountOfDays = (date: Date) => {
+  let nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1);
 };
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
