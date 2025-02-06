1b87025e49e7e5f61da8dfd86ce5ca15e034fb34
barreledout <rcalisaan7@gmail.com>
2025-02-06 12:24:35 -0800

    Added the home price to the pagination component rather than it be in the Comparables component for styling.

--- client/src/components/ui/pagination.tsx
+++ client/src/components/ui/pagination.tsx
@@ -13,9 +13,8 @@ const Pagination = ({
   propertyType,
   totalComparable,
 }: PaginationProps) => {
-
   // Total amount of results to show per page.
-  const POSTPERPAGE = 6
+  const POSTPERPAGE = 6;
   const [postPerPage, setPostPerPage] = useState<number>(POSTPERPAGE);
 
   // Checks if the return data size is less than the user's
@@ -23,7 +22,7 @@ const Pagination = ({
   const [equalAmountComps, setEqualAmountComps] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const paginationNumbers: number[] = [];
-  
+
   // filters result to property type input
   const filteredResults = resultsArr.comparables.filter(
     (property) => property.propertyType === propertyType
@@ -54,21 +53,53 @@ const Pagination = ({
 
   return (
     <>
+      <div
+        className={`${
+          filteredResults.length === 0 ? "hidden" : "block"
+        } text-center font-nunito my-16`}
+      >
+        <h1 className="text-3xl font-[700] md:text-3xl lg:text-4xl">
+          Home Estimate:
+        </h1>
+        <div>
+          <h2 className="pt-2 text-3xl text-blue-600 font-[600] font-geistSans md:text-4xl">
+            {resultsArr.price
+              ? `$${resultsArr.price.toLocaleString()}`
+              : "Price Not Available"}
+          </h2>
+        </div>
+      </div>
+
       {/* Comparable Property Results */}
       <div className="max-w-[950px] mx-auto">
         <div className="text-center grid gap-1">
           <h1 className="text-2xl font-[700] pb-3 text-center font-nunito md:text-3xl lg:text-4xl">
             Comparable Properties
           </h1>
+
+          {/* If the filtered result's length > 0 but !== user inputted amount */}
           <h2
             className={`${
-              !equalAmountComps ? "block" : "hidden"
+              !equalAmountComps && filteredResults.length > 0
+                ? "block"
+                : "hidden"
             } font-nunito text-lg fpn pb-2`}
           >
             Only found{" "}
             <span className="font-[700]">{filteredResults.length}</span> out of{" "}
             <span className="font-[700]">{totalComparable}</span> {propertyType}{" "}
             properties to compare.
+          </h2>
+
+          {/* If it returns no properties */}
+          <h2
+            className={`${
+              filteredResults.length === 0 ? "block" : "hidden"
+            } font-nunito text-2xl fpn pb-2`}
+          >
+            Found <span className="font-[700]">{filteredResults.length}</span>{" "}
+            out of <span className="font-[700]">{totalComparable}</span>{" "}
+            {propertyType} properties to compare.
           </h2>
         </div>
 
@@ -129,13 +160,20 @@ const Pagination = ({
       </div>
 
       {/* Pagination Button Bar  */}
-      <div className={`${paginationNumbers.length < 2 ? "hidden" : "block"} flex justify-center mt-4 gap-2 font-nunito`}>
+      <div
+        className={`${
+          paginationNumbers.length < 2 ? "hidden" : "block"
+        } flex justify-center mt-4 gap-2 font-nunito`}
+      >
         {paginationNumbers.map((pageNumber) => (
-          <button 
-            key={pageNumber} 
+          <button
+            key={pageNumber}
             onClick={() => setCurrentPage(pageNumber)}
-            className={`${pageNumber === currentPage ? "bg-blue-300/30 font-[700] rounded-sm shadow" : "bg-none"} px-2 py-0.5`}
-          
+            className={`${
+              pageNumber === currentPage
+                ? "bg-blue-300/30 font-[700] rounded-sm shadow"
+                : "bg-none"
+            } px-2 py-0.5`}
           >
             {pageNumber}
           </button>
