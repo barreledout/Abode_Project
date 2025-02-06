dd3c304f65084db74f81d7b2700ec560eaccf1e1
barreledout <rcalisaan7@gmail.com>
2025-02-05 16:32:40 -0800

    Pagination is complete

--- client/src/Pages/HomeEstimate/Comparables.tsx
+++ client/src/Pages/HomeEstimate/Comparables.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { forwardRef, useEffect, useState } from "react";
 import { ApiResponse } from "./HomeTypes";
+import Pagination from "../../components/ui/pagination";
 
 type ComparableProps = {
   data: ApiResponse;
@@ -10,22 +11,7 @@ type ComparableProps = {
 
 const Comparables = forwardRef<HTMLElement, ComparableProps>(
   ({ data, propertyType, totalComparable }, ref) => {
-    const [equalAmountComps, setEqualAmountComps] = useState<boolean>(true);
-
-    // filters result to property type input
-    const filteredResults = data.comparables.filter(
-      (property) => property.propertyType === propertyType
-    );
-
-    // If filtered result length is less than comp count input, notify user
-    useEffect(() => {
-      if (filteredResults.length !== totalComparable) {
-        setEqualAmountComps(false);
-      } else {
-        setEqualAmountComps(true);
-      }
-    }, [filteredResults.length, totalComparable]);
-
+    
     return (
       <section id="comparables" ref={ref}>
         <div>
@@ -43,79 +29,9 @@ const Comparables = forwardRef<HTMLElement, ComparableProps>(
           </div>
 
           {/* API Results */}
-          <div className="max-w-[950px] mx-auto">
-            <div className="text-center grid gap-1">
-              <h1 className="text-2xl font-[700] pb-3 text-center font-nunito md:text-3xl lg:text-4xl">
-                Comparable Properties
-              </h1>
-              <h2
-                className={`${
-                  !equalAmountComps ? "block" : "hidden"
-                } font-nunito text-lg fpn pb-2`}
-              >
-                Only found{" "}
-                <span className="font-[700]">{filteredResults.length}</span> out
-                of <span className="font-[700]">{totalComparable}</span>{" "}
-                {propertyType} properties to compare.
-              </h2>
-            </div>
-
-            {/* Comparable Property Results */}
-            <div className="">
-              <ul className="flex flex-col gap-2 mx-2 pt-2 mq400w:grid mq400w:grid-cols-2 md:grid md:grid-cols-3 md:px-3">
-                {filteredResults.map((comparable) => (
-                  <li
-                    key={comparable.id}
-                    className="grid bg-blue-300/30 shadow p-2 rounded-sm font-nunito"
-                  >
-                    <span className="">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Address:
-                      </span>{" "}
-                      {comparable.formattedAddress}
-                    </span>
-                    <span className="">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Price:
-                      </span>{" "}
-                      {`$${comparable.price.toLocaleString()}`}
-                    </span>
-                    <span className="hidden md:block">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Property Type:
-                      </span>{" "}
-                      {comparable.propertyType}
-                    </span>
-                    <span className="hidden md:block">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Bedrooms:
-                      </span>{" "}
-                      {comparable.bedrooms}
-                    </span>
-                    <span className="hidden md:block">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Bathrooms:
-                      </span>{" "}
-                      {comparable.bathrooms}
-                    </span>
-                    <span className="hidden md:block">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Square Footage:
-                      </span>{" "}
-                      {comparable.squareFootage}
-                    </span>
-                    <span className="">
-                      <span className="font-[600] font-geistSans lg:text-lg ">
-                        Correlation:
-                      </span>{" "}
-                      {(comparable.correlation * 100).toFixed(2) + "%"}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+          <Pagination resultsArr={data} propertyType={propertyType} totalComparable={totalComparable}/>
         </div>
+        
       </section>
     );
   }
---
+++ client/src/components/ui/pagination.tsx
@@ -0,0 +1,147 @@
+"use client";
+import { useState, useEffect, useMemo } from "react";
+import { ApiResponse, Comparable } from "@/Pages/HomeEstimate/HomeTypes";
+
+interface PaginationProps {
+  resultsArr: ApiResponse;
+  propertyType: String | null;
+  totalComparable: number | null;
+}
+
+const Pagination = ({
+  resultsArr,
+  propertyType,
+  totalComparable,
+}: PaginationProps) => {
+  const [postPerPage, setPostPerPage] = useState<number>(5);
+  const paginationNumbers: number[] = [];
+  const [equalAmountComps, setEqualAmountComps] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // filters result to property type input
+  const filteredResults = resultsArr.comparables.filter(
+    (property) => property.propertyType === propertyType
+  );
+
+  // If filtered result length is less than comp count input, notify user
+  useEffect(() => {
+    if (filteredResults.length !== totalComparable) {
+      setEqualAmountComps(false);
+    } else {
+      setEqualAmountComps(true);
+    }
+  }, [filteredResults.length, totalComparable]);
+
+  // Total amount of pages
+  for (
+    let i: number = 1;
+    i <= Math.ceil(filteredResults.length / postPerPage);
+    i++
+  ) {
+    paginationNumbers.push(i);
+  }
+
+  // Paginated results array.
+  const paginatedResults = useMemo(() => {
+    return postPerPageFn(filteredResults, currentPage, postPerPage);
+  }, [filteredResults, currentPage, postPerPage]);
+
+  return (
+    <>
+      <div className="max-w-[950px] mx-auto">
+        <div className="text-center grid gap-1">
+          <h1 className="text-2xl font-[700] pb-3 text-center font-nunito md:text-3xl lg:text-4xl">
+            Comparable Properties
+          </h1>
+          <h2
+            className={`${
+              !equalAmountComps ? "block" : "hidden"
+            } font-nunito text-lg fpn pb-2`}
+          >
+            Only found{" "}
+            <span className="font-[700]">{filteredResults.length}</span> out of{" "}
+            <span className="font-[700]">{totalComparable}</span> {propertyType}{" "}
+            properties to compare.
+          </h2>
+        </div>
+
+        {/* Comparable Property Results */}
+        <div className="">
+          <ul className="flex flex-col gap-2 mx-2 pt-2 mq400w:grid mq400w:grid-cols-2 md:grid md:grid-cols-3 md:px-3">
+            {paginatedResults.map((comparable) => (
+              <li
+                key={comparable.id}
+                className="grid bg-blue-300/30 shadow p-2 rounded-sm font-nunito"
+              >
+                <span className="">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Address:
+                  </span>{" "}
+                  {comparable.formattedAddress}
+                </span>
+                <span className="">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Price:
+                  </span>{" "}
+                  {`$${comparable.price.toLocaleString()}`}
+                </span>
+                <span className="hidden md:block">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Property Type:
+                  </span>{" "}
+                  {comparable.propertyType}
+                </span>
+                <span className="hidden md:block">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Bedrooms:
+                  </span>{" "}
+                  {comparable.bedrooms}
+                </span>
+                <span className="hidden md:block">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Bathrooms:
+                  </span>{" "}
+                  {comparable.bathrooms}
+                </span>
+                <span className="hidden md:block">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Square Footage:
+                  </span>{" "}
+                  {comparable.squareFootage}
+                </span>
+                <span className="">
+                  <span className="font-[600] font-geistSans lg:text-lg ">
+                    Correlation:
+                  </span>{" "}
+                  {(comparable.correlation * 100).toFixed(2) + "%"}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* Show pagination buttons  */}
+      <div className="flex justify-center mt-4 gap-2">
+        {paginationNumbers.map((pageNumber) => (
+          <button key={pageNumber} onClick={() => setCurrentPage(pageNumber)}>
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
+
+// Paginates the array and slices the results to its respective page.
+const postPerPageFn = (
+  properties: Comparable[],
+  currentPage: number,
+  postPerPage: number
+) => {
+  const startIndex = (currentPage - 1) * postPerPage;
+  const endIndex = startIndex + postPerPage;
+  return properties.slice(startIndex, endIndex);
+};
