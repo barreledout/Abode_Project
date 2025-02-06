49cfa5864bb2dd8709af5a051e6cb54771596551
barreledout <rcalisaan7@gmail.com>
2025-02-06 12:25:10 -0800

    Moved the home price container to the pagination component for styling.

--- client/src/Pages/HomeEstimate/Comparables.tsx
+++ client/src/Pages/HomeEstimate/Comparables.tsx
@@ -11,27 +11,14 @@ type ComparableProps = {
 
 const Comparables = forwardRef<HTMLElement, ComparableProps>(
   ({ data, propertyType, totalComparable }, ref) => {
-    
     return (
-      <section id="comparables" ref={ref}>
-        <div>
-          <div className="text-center font-nunito my-16">
-            <h1 className="text-3xl font-[700] md:text-3xl lg:text-4xl ">
-              Home Estimate:
-            </h1>
-            <div>
-              <h2 className="pt-2 text-3xl text-blue-600 font-[600] font-geistSans md:text-4xl">
-                {data.price
-                  ? `$${data.price.toLocaleString()}`
-                  : "Price Not Available"}
-              </h2>
-            </div>
-          </div>
-
-          {/* API Results */}
-          <Pagination resultsArr={data} propertyType={propertyType} totalComparable={totalComparable}/>
-        </div>
-        
+      <section id="comparables" ref={ref}>  
+        {/* API Results */}
+        <Pagination
+          resultsArr={data}
+          propertyType={propertyType}
+          totalComparable={totalComparable}
+        />      
       </section>
     );
   }
